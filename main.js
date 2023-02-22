d066f9c466d1eef8f5e31e4308a49e94ba205bc9
zawho <zthooper2@gmail.com>
2023-02-22 11:53:25 +0800

    Update game board create func

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ const gameBoard = (() => {
     const rows = 3;
     const columns = 3;
     const board = [];
-    for (let i = 0; i < rows; i++) {
-        board[i] = i;
-        for (let j = 0; j < columns; j++) {
-            board[j] = j;
-        }
+    function createGameBoard() {
+        for (let i = 0; i < rows; i++) {
+            board[i] = [];
+            for (let j = 0; j < columns; j++) {
+                board[j] = j;
+            }
+        };
     };
+    createGameBoard();
     return {
         board
     };

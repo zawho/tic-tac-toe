7f08c2dba40f9e782921c69c968df6a9f27dc629
zawho <zthooper2@gmail.com>
2023-02-22 15:30:23 +0800

    Add HTML for game board

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const gameBoardDiv = document.querySelector('.game-board');
+
 // Game board module.
 const gameBoard = (() => {
     const rows = 3;
@@ -6,8 +8,20 @@ const gameBoard = (() => {
     function createGameBoard() {
         for (let i = 0; i < rows; i++) {
             board[i] = [];
+            const boardRow = document.createElement('div');
+            boardRow.style.display = 'flex';
+            gameBoardDiv.appendChild(boardRow);
             for (let j = 0; j < columns; j++) {
-                board[j] = j;
+                board[j] = i;
+                const boardCell = document.createElement('div');
+                boardCell.style.width = '30px';
+                boardCell.style.height = '30px';
+                boardCell.style.border = 'solid 1px black';
+                boardCell.style.display = 'flex';
+                boardCell.style.justifyContent = 'center';
+                boardCell.style.alignItems = 'center';
+                boardCell.innerText = board[j];
+                boardRow.appendChild(boardCell);
             }
         };
     };
@@ -27,3 +41,5 @@ const playerTwo = playerFactory('Player 2');
 const gameFlow = (() => {
 
 })();
+
+

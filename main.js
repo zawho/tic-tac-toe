e61fae6c240bcdd9abf33633e9a5a4d9b9b74a8b
zawho <zthooper2@gmail.com>
2023-02-23 09:36:48 +0800

    Fix board array and add basic turn func

--- main.js
+++ main.js
@@ -2,36 +2,43 @@ const gameBoardDiv = document.querySelector('.game-board');
 
 // Game board module.
 const gameBoard = (() => {
-    const boardCell = 9;
     const board = [];
+    let boardCellDiv;
     function createGameBoard() {
-        for (let i = 0; i < boardCell; i++) {
-            const boardCellDiv = document.createElement('div');
+        for (let i = 0; i < 9; i++) {
+            boardCellDiv = document.createElement('div');
             boardCellDiv.style.border = 'solid 1px black';
             boardCellDiv.style.display = 'flex';
             boardCellDiv.style.justifyContent = 'center';
             boardCellDiv.style.alignItems = 'center';
             gameBoardDiv.appendChild(boardCellDiv);
+            board.push(boardCellDiv);
         };
+        console.log(board);
     };
     createGameBoard();
     return {
-        board
+        board,
+        boardCellDiv
     };
 })();
 
 // Player factory function and player variables.
 const playerFactory = (playerName, playerSymbol) => {
     function takeTurn() {
-
+        for (let i = 0; i < gameBoard.board.length; i++) {
+            gameBoard.board[i].innerText = 'X';
+        }
     }
     return {
         playerName,
-        playerSymbol
+        playerSymbol,
+        takeTurn
     }
 };
 
 const playerOne = playerFactory('Player 1', 'X');
+playerOne.takeTurn();
 const playerTwo = playerFactory('Player 2', 'O');
 
 // Game flow module.
